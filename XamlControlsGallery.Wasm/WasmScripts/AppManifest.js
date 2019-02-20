1bea958dc53ab085ba57e75940eceae7db99d140
Jérôme Laban <jerome.laban@nventive.com>
2019-02-19 22:21:23 -0500

    Adjust XAML casing

--- XamlControlsGallery.Wasm/WasmScripts/AppManifest.js
+++ XamlControlsGallery.Wasm/WasmScripts/AppManifest.js
@@ -2,5 +2,5 @@
 
     splashScreenImage: "Assets/Tiles/splash-sdk.scale-400.png",
     splashScreenColor: "#fff",
-    displayName: "Xaml Controls Gallery"
+    displayName: "XAML Controls Gallery"
 };
