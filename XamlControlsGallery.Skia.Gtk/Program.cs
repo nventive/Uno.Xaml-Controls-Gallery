7aa95b58cd1047c366fc6197cd01356d8b00f92b
Jerome Laban <jerome.laban@nventive.com>
2020-08-11 21:02:44 -0400

    Skia WPF

---
+++ XamlControlsGallery.Skia.Gtk/Program.cs
@@ -0,0 +1,24 @@
+﻿using GLib;
+using System;
+using System.Collections;
+using Uno.Extensions;
+using Uno.Foundation.Extensibility;
+
+namespace AppUIBasics.Wasm
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ExceptionManager.UnhandledException += delegate (UnhandledExceptionArgs expArgs)
+			{
+				Console.WriteLine("GLIB UNHANDLED EXCEPTION" + expArgs.ExceptionObject.ToString());
+				expArgs.ExitApplication = true;
+			};
+
+			var host = new Uno.UI.Runtime.Skia.GtkHost(() => new App(), args);
+
+			host.Run();
+		}
+	}
+}
